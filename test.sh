f7b631d26aee0b1537e6d0308140e1d168c398b4
ISSSSS212 <55965496+ISSSSS212@users.noreply.github.com>
2025-04-10 20:27:16 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-sudo apt update && sudo apt install tigervnc-standalone-server qemu-kvm firefox openbox lxde neofetch kitty -y
+sudo apt update && sudo apt install tigervnc-standalone-server qemu-kvm firefox openbox xfce4 xfce4-goodies neofetch kitty -y
 git clone https://github.com/novnc/noVNC.git
 cd noVNC
 sudo vncserver -SecurityTypes None -rfbport 5080 
