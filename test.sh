ab094dafa7162dcb8484ebc2811b79d2af429e0d
ISSSSS212 <55965496+ISSSSS212@users.noreply.github.com>
2025-04-11 14:36:45 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
-sudo apt update && sudo apt install tigervnc-standalone-server qemu-kvm firefox openbox xfce4 xfce4-goodies neofetch kitty -y
+sudo apt update && sudo apt install tigervnc-standalone-server qemu-kvm firefox openbox neofetch kitty -y
+echo "if you want to install a desktop, run sudo apt install lxqt or sudo apt install xfce4 xfce4-goodies"
 git clone https://github.com/novnc/noVNC.git
 cd noVNC
 sudo vncserver -SecurityTypes None -rfbport 5080 
