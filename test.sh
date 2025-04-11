b7996ecb667fe5d17dad0cc186cae9877e356251
ISSSSS212 <55965496+ISSSSS212@users.noreply.github.com>
2025-04-10 19:25:03 -0600

    Update test.sh
    changed from KDE to lubuntu :)

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/bash
-sudo apt update && sudo apt install tigervnc-standalone-server qemu-kvm firefox kde-plasma-desktop neofetch kitty -y
+sudo apt update && sudo apt install tigervnc-standalone-server qemu-kvm firefox openbox lubuntu-desktop neofetch kitty -y
 git clone https://github.com/novnc/noVNC.git
 cd noVNC
-sudo vncpasswd 123456
-sudo vncserver -rfbport 5080 
+sudo vncserver -SecurityTypes None -rfbport 5080 
 ./utils/novnc_proxy --vnc 127.0.0.1:5080 --listen localhost:8000
